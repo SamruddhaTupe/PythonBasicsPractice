b6f224e43a79edacc08901b40d7e1e8fc54e859b
SamruddhaTupe <85238567+SamruddhaTupe@users.noreply.github.com>
2022-03-18 00:00:46 +0530

    Update Switching_words_in_a_list.py

--- Switching_words_in_a_list.py
+++ Switching_words_in_a_list.py
@@ -1,18 +1,15 @@
 def listrev(thelist):
     astring = ''.join(thelist) #Converting the list into a string.
-    alist = astring.split(' ') #Converting the string into a string but with words as elements as opposed to letters as elements.
+    alist = astring.split(' ') #Converting the string into a list but with words as elements as opposed to letters as elements.
     thefinallist = [] #A list that is used in the for loop to interchange the words as per the request.
     #For loop to interchange the words.
     for i in range(0, len(alist)):
         thefinallist.append(alist[len(alist)-i-1])
         if i != (len(alist)-1):
             thefinallist.append(' ')
-    #Converting the list into string.
+    #Converting the list into a string.
     finalstring = ''.join(thefinallist)
-    #Converting a string into a list but with characters as elements.
-    output = list(finalstring.strip(" "))
-    return output
+    #Converting the string into a list but with characters as elements.
+    return list(finalstring.strip(" "))
 alist = ['p', 'e', 'r', 'f', 'e', 'c', 't', ' ', 'm', 'a', 'k', 'e', 's', ' ', 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e']
 print(listrev(alist))
-
-
